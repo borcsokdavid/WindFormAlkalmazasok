603c1dc34a8db7a3695a7ed0b2c10c935f99d164
borcsokdavid <borcsokdavid@gmail.hu>
2020-03-09 13:01:34 +0100

    sda

---
+++ Snooker/Snooker/Form1.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace Snooker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "2.Feladat: Versenyzőlista:";
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(15, 54);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(368, 182);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "";
+            this.columnHeader1.Width = 77;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "";
+            this.columnHeader2.Width = 104;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "";
+            this.columnHeader3.Width = 95;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "";
+            this.columnHeader4.Width = 119;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 242);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Kijelölt törlése";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(160, 242);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(139, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Összes törlése";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(437, 99);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(434, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "4.Feladat";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(437, 139);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 27);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Számítás";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(427, 185);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 27);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Számítás";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(451, 169);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "5.Feladat";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(508, 189);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(288, 20);
+            this.textBox2.TabIndex = 7;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(597, 26);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 10;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(741, 24);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "Szűrés";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(741, 78);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 13;
+            this.button6.Text = "Szűrés";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(597, 80);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 14;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(427, 255);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 15;
+            this.button7.Text = "Mennyi";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(508, 258);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(288, 20);
+            this.textBox4.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(857, 348);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ Snooker/Snooker/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Snooker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string elsoSor = "";
+        List<versenyzo> versenyzoLista = new List<versenyzo>();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            using (StreamReader sr = new StreamReader("snooker.txt", Encoding.UTF8))
+            {
+                elsoSor = sr.ReadLine();
+                while (!sr.EndOfStream)
+                {
+                    string sor = sr.ReadLine();
+                    for (int i = 0; i < elsoSor.Split(';').Length; i++)
+                    {
+                        listView1.Columns[i].Text = elsoSor.Split(';')[i];
+                    }
+                    
+                    
+                    int helyezes = Int32.Parse(sor.Split(';')[0]);
+                    string nev = sor.Split(';')[1];
+                    string orszag = sor.Split(';')[2];
+                    int nyeremeny = Int32.Parse(sor.Split(';')[3]);
+                    versenyzo Versenyzo = new versenyzo(helyezes, nev, orszag, nyeremeny);
+                    versenyzoLista.Add(Versenyzo);
+                    ListViewItem lvi = new ListViewItem(helyezes.ToString());
+                    lvi.SubItems.Add(nev);
+                    lvi.SubItems.Add(orszag);
+                    lvi.SubItems.Add(nyeremeny.ToString());
+                    listView1.Items.Add(lvi);
+
+                    /*columnHeader1.Text = elsoSor.Split(';')[0];
+                    columnHeader2.Text = elsoSor.Split(';')[1];
+                    columnHeader3.Text = elsoSor.Split(';')[2];
+                    columnHeader4.Text = elsoSor.Split(';')[3];*/
+
+                    bool vanE = true;
+                    foreach (var item in comboBox1.Items)
+                    {
+                        if (item.ToString() == orszag)
+                        {
+                            vanE = false;
+                                break;
+                        }
+                    }
+                    if (vanE)
+                    {
+                        comboBox1.Items.Add(orszag);
+                    }
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            foreach (ListViewItem item in listView1.SelectedItems)
+            {
+                listView1.Items.Remove(item);
+            }
+            
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            int osszeg = 0;
+            foreach (ListViewItem item in listView1.Items)
+            {
+                osszeg += int.Parse(item.SubItems[3].Text);
+            }
+            int atlag = osszeg / listView1.Items.Count;
+            textBox1.Text = atlag.ToString();
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int osszeg = 0;
+            int index = -1;
+            foreach (ListViewItem item in listView1.Items)
+            {
+                if (item.SubItems[2].Text == "Kína" && int.Parse(item.SubItems[3].Text) > osszeg)
+                {
+                    osszeg = int.Parse(item.SubItems[3].Text);
+                    index = item.Index;
+                }
+            }
+            foreach  (ListViewItem item in listView1.Items)
+            {
+                if (item.Index == index)
+                {
+                    string helyezes = item.SubItems[0].Text;
+                    string nev = item.SubItems[1].Text;
+                    string orszag = item.SubItems[2].Text;
+                    string nyeremeny = item.SubItems[3].Text;
+
+                    textBox2.Text = $"{helyezes}, {nev}, {orszag}, {nyeremeny}"; 
+                }
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            int ertek = Int32.Parse(textBox3.Text);
+            foreach (ListViewItem item in listView1.Items)
+            {
+                if (item.SubItems[2].Text != comboBox1.SelectedItem.ToString() || Int32.Parse(item.SubItems[3].Text) < ertek)
+                {
+                    listView1.Items.Remove(item);
+                }
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            
+            foreach (ListViewItem item in listView1.Items)
+            {
+                if (Int32.Parse(item.SubItems[3].Text) < Int32.Parse( textBox3.Text))
+                {
+                    listView1.Items.Remove(item);
+                }
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
